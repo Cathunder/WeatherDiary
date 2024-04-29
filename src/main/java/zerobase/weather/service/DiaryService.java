6a412c15c3ac48017fbfb19c343d33dc407cf453
takealook <macaroonv@gmail.com>
2024-04-30 04:16:43 +0900

    openWeatherMap 사이트에서 데이터 가져오는 로직 추가

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -3,6 +3,11 @@ package zerobase.weather.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDate;
 
 @Service
@@ -13,12 +18,40 @@ public class DiaryService {
     private String apiKey;
 
     public void createDiary(LocalDate date, String text) {
-        getWeatherString();
+        // open weather map에서 날씨 데이터 가져오기
+        String weatherData = getWeatherString();
+
+        // 받아온 날씨 json 파싱하기
+
+        // 파싱된 데이터 + 일기값 우리 db에 넣기
+
     }
 
     private String getWeatherString() {
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
-        System.out.println(apiUrl);
-        return "";
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // url을 http형식으로 연결
+            connection.setRequestMethod("GET"); // get으로 호출
+            int responseCode = connection.getResponseCode(); // 요청에 대한 응답 결과를 코드로 받아옴
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            }
+
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            return response.toString();
+        } catch (Exception e) {
+            return "failed to get response";
+        }
     }
 }
